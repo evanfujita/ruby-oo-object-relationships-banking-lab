666359f7120e8f7552d154865b1c60b81d0735bb
Evan <evanfujita@gmail.com>
2020-12-13 16:55:04 -0600

    complete

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,6 @@ require_relative 'bank_account.rb'
 
 class Transfer
   
-  @@count = 1
 
   attr_accessor :sender, :receiver, :amount, :status, :balance
   attr_reader :name
@@ -13,25 +12,47 @@ class Transfer
     @receiver = receiver
     @amount = amount
     @status = "pending"
+    @count = 1
   end
 
   def valid?
     self.sender.valid? && self.receiver.valid?
   end
 
+  def bad_transaction
+    if !self.valid?
+      self.status = "rejected"
+    end
+    return "Transaction rejected. Please check your account balance."
+  end
+
+  def good_transaction
+    if self.valid?
+    while @count > 0 do
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+      @count -= 1
+    end
+    end
+  end  
+
   def execute_transaction
-        self.sender.balance -= self.amount
-        self.receiver.balance += self.amount
-        self.status = "complete"
-        @@count -= 1
-        
-      end
- 
+    self.good_transaction || self.bad_transaction
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+    self.sender.balance += self.amount
+    self.receiver.balance -= self.amount    
+    self.status = "reversed"
+    end
+  end
+
 end
 
 
 marty = BankAccount.new("Marty")
 evan = BankAccount.new("Evan")
-transfer = Transfer.new(marty, evan, 500)
+transfer = Transfer.new(marty, evan, 1500)
 
-#binding.pry
